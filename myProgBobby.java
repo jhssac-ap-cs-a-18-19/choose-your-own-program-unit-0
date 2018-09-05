007b1797800862a5bd709cdfd448ee55908b47cc
canalasr <riverat303@yahoo.com>
2018-09-05 20:03:51 +0000

    final product

--- myProgBobby.java
+++ myProgBobby.java
@@ -3,6 +3,22 @@ import java.io.*;
 public class myProgBobby {
     public static void main(String[] args) {
         //declare vars
-        int
+        int baseCost;
+        String itemName;
+        final double caTax = 1.075;
+        double whatTax;
+        //Scanner
+        Scanner in = new Scanner(System.in);
+        System.out.print("What are you trying to buy?\n");
+        itemName = in.nextLine();
+        System.out.print("How much does it cost?\n");
+        baseCost = in.nextInt();
+        System.out.print("What is your tax rate?\n");
+        whatTax = in.nextDouble();
+        double caCost = ((double) baseCost * caTax);
+        double infidelCost = ((double) baseCost * whatTax);
+        System.out.print("In California, your " + itemName + " costs $" + caCost + ".\n");
+        System.out.print("In your inferior sales tax land, your " + itemName + " costs $" + infidelCost + ".\n");
+        System.out.print("The price difference is $" + (caCost - infidelCost) + ".\n");
     }
 }
